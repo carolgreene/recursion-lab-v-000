6163af9991e2d79c9469a96db7ef79c382790c13
carolgreene <carolgreene3@gmail.com>
2020-10-31 06:06:57 -0700

    added Palidrome function

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Code your solution here!
+//print individual chars of a string
 function printString(myString) {
   console.log(myString[0])
     if (myString.length > 1) {
@@ -13,8 +14,9 @@ function printString(myString) {
     }
   }
 
+//reverse string
   function reverseString(myString) {    
-      console.log('myStr', myStr)
+      console.log('myStr', myString)
       //base case to end recursion
         if (myStr === '') {
           return '';
@@ -24,12 +26,31 @@ function printString(myString) {
         //+ myStr.charAt(0) will add the first letter
         //on to the end of the string.
         console.log('substr', myStr.substr(1))
-        console.log('charAt0', myStr.charAt(0))
-        
+        console.log('charAt0', myStr.charAt(0))        
         return reverseStr(myStr.substr(1)) + myStr.charAt(0)
         
         }
       }
+
+      //is the string a palidrome?
+      function isPalidrome(str) {
+        console.log(str)
+        //base case to end recursion
+        //is a palidrome if it's down to 0 or 1 characters
+        if (str.length <= 1) {
+          return true
+          //ck 1st char & last char to see if same
+        } else if (str[0] !== str.charAt(str.length - 1)) {
+          return false
+        }
+        //remove the 1st & last char & call function again
+        //combined the line below into the return line
+        //str = str.substr(1, str.length - 2)
+        return isPalidrome(str.substr(1, str.length - 2))
+      }
+      
+      
+      
       
       
   
